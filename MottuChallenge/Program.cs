1a294605e4e4e2baf9510b294ba28e0d20720def
cadenasza <leonardocadena2005@gmail.com>
2025-05-25 18:56:55 -0300

    adicionando commentrarios no swagger

--- MottuChallenge/Program.cs
+++ MottuChallenge/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using MottuChallenge.Data;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +12,28 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseOracle(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "MottuChallenge API",
+        Version = "v1",
+        Description = "API RESTful para gerenciamento de motos e ocupações de vagas.",
+        Contact = new OpenApiContact
+        {
+            Name = "Leonardo Cadena de Souza | Davi Gonzaga Ferreira | Julia Vasconcelos Oliveira",
+            Email = "leonardocadenasza@gmail.com"
+        },
+        License = new OpenApiLicense
+        {
+            Name = "MIT",
+            Url = new Uri("https://opensource.org/licenses/MIT")
+        }
+    });
+
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    c.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
 
 var app = builder.Build();
 
